9e899dcb9823282156b98aaf61a1f510032e0802
Benaka R <benakar2514111118@gmail.com>
2024-07-24 23:32:03 +0530

    Upgraded the custom encryption to fernet encryption

--- capture/views.py
+++ capture/views.py
@@ -3,68 +3,22 @@ from django.core.files.storage import FileSystemStorage
 from .match import match_iris
 import pathlib
 from random import randint
+from cryptography.fernet import Fernet
 
-def dec_rtrn(text):
-    res=""
-    m=n=0
-    ln=len(text)
-    k1=text[ln-1:ln-3:-1]
-    k=k2=bits=""
-    rl=0
-    if k1[0]<k1[1]:
-        rl=0
-        lnt=ord(k1[1])-ord(k1[0])
-        k=text[ln-1:ln-1-lnt:-1]
-        m = ord(k[lnt-2]) - ord('m')
-        n = ord(k[lnt-1]) - ord('n')
-        k2=k[2:lnt-2]
-    else:
-        rl=1
-        k1=k1[::-1]
-        lnt=ord(k1[1])-ord(k1[0])
-        k=text[ln-1:ln-1-lnt:-1]
-        m = ord(k[lnt-1]) - ord('m')
-        n = ord(k[lnt-2]) - ord('n')
-        k2=k[2:lnt-2]
-    
-    for l in k2:
-        dif=ord(l)-ord('A')
-        tmp=""
-        for i in range(5):
-            tmp+=str(dif%2)
-            dif=dif//2
-        tmp=tmp[::-1]
-        if rl==0:
-            st=tmp[4]
-            st+=tmp[0:4]
-            tmp=st
-        else:
-            st=tmp[1:]
-            st+=tmp[0]
-            tmp=st
-        bits=bits+tmp
-        
-    diff=ord(k1[0])-ord('a')
-    bits=bits[ 0 : len(bits)-diff ]
-    
-    msg=""
-    for i in range(len(bits)):
-        if bits[i]=='0':
-            msg += chr(ord(text[i])-m)
-        else:
-            msg += chr(ord(text[i])-n)
-    res=msg
-    return res
+#key = Fernet.generate_key()
+key = b'bY7JXmRqIyzj4kdkc_YBwJwzTOlc2WEfflGx8RauxaQ='
+
+fnet = Fernet(key)
+
 
 def match(request):
     if request.method == 'POST':
         file = request.FILES['imgfile']
         Org_data = file.read()
         file.close()
-        dc=dec_rtrn(Org_data.decode())
-        Byte_data=bytes.fromhex(dc)
-        with open("static/output.jpg",'wb') as file: 
-            file.write(Byte_data)
+        decrypted_data = fnet.decrypt(Org_data)
+        with open("static/output.jpg", "wb") as file:
+            file.write(decrypted_data)
         result = match_iris("output.jpg")
         if result[1] > 40:
             return HttpResponse("Best Match: " + str(result[0]) +"\n"+ str(round(result[1],4)) +"  %")
@@ -89,11 +43,9 @@ def new(request):
         if pathlib.Path("static/iris_data/"+name+".jpg").is_file():
             return HttpResponse("Data already exists")
         Org_data = file.read()
-        dc=dec_rtrn(Org_data.decode())
-        Byte_data=bytes.fromhex(dc)
-        with open("static/iris_data/"+name+".jpg",'wb') as file: 
-            file.write(Byte_data)
-
+        decrypted_data = fnet.decrypt(Org_data)
+        with open("static/iris_data/"+name+".jpg", "wb") as file:
+            file.write(decrypted_data)
         if pathlib.Path("static/iris_data/"+name+".jpg").is_file():
             return HttpResponse("Data Saved for "+ name +" Successfully")
         else:
--- raspberry_code.py
+++ raspberry_code.py
@@ -1,10 +1,9 @@
 from picamera2 import Picamera2
+from cryptography.fernet import Fernet
 import cv2
 import tkinter as tk
-from PIL import Image, ImageTk
 import requests
 import os 
-from random import randint
 import signal
 import atexit
 def on_exit():
@@ -19,59 +18,16 @@ signal.signal(signal.SIGINT,on_exit)
 url = "http://your_device_ip_where_django_server_runs:8000/"
 s = requests.Session()
 
-def enc_rtrn(text):
-    res=""
-    m=randint(1,5)
-    n=randint(1,5)
-    while m==n:
-        n=randint(1,5)
-    k=k1=k2=k3=""
-    rl=randint(0,1)
-    res=""
-    bits=""
-    for i in range(len(text)):
-        r=randint(0,1)
-        bits+=str(r)
-    i=0
-    while i<len(bits):
-        t=bits[i:i+5]
-        while len(t)<5:
-            t+='0'
-        if rl==0:
-            st=t[1:]
-            st+=t[0]
-            t=st
-        else:
-            st=t[4]
-            st+=t[0:4]
-            t=st
-        num=int(t)
-        k2tmpint=0
-        j=0
-        while num!=0:
-            k2tmpint += (num%10)*(2**j)
-            num=num//10
-            j+=1
-        k2=k2+chr(ord('A') + k2tmpint)
-        i+=5
-        if i>=len(bits):
-            k1=chr(ord('a')+(i-len(bits)))
+if os.path.exists("output.jpg"):
+	os.remove("output.jpg")
+if os.path.exists("encoded.jpg"):
+	os.remove("encoded.jpg")
 
-    msg=""
-    for i in range(len(text)):
-        if bits[i]=='0':
-            msg += chr(ord(text[i])+m)
-        else:
-            msg += chr(ord(text[i])+n)
-    if rl==0:
-        k1 = k1 + chr(ord(k1[0])+len(k2)+4)
-        k3=chr(ord('m')+m) + chr(ord('n')+n)
-    else:
-        k1 = chr(ord(k1[0])+len(k2)+4) + k1
-        k3 = chr(ord('n')+n) + chr(ord('m')+m)
-    k=k1+k2+k3
-    res=msg+k[::-1]
-    return res
+#key = Fernet.generate_key()     #--This will generate a unique key for both encryption and decryption. Below is a generated key which will be constant for all transactions.
+key = b'bY7JXmRqIyzj4kdkc_YBwJwzTOlc2WEfflGx8RauxaQ='         # It is NOT AT ALL SAFE to use here. But I have used it directly for the time being.
+
+
+fnet = Fernet(key)
 
 window=tk.Tk()
 window.geometry('800x800+600+200')
@@ -92,15 +48,11 @@ def req_new():
 	def inserts(event=None):
 		text = e.get()
 		try:
-			img = cv2.imread("output.jpg")
-			img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-			cv2.imwrite("output.jpg",img)
 			with open('output.jpg','rb') as file:
 				Org_data = file.read()
-				String_data=str(Org_data.hex())
-				String_data_enc=enc_rtrn(String_data)
+				Enc_data = fnet.encrypt(Org_data)
 				with open('encoded.jpg','wb') as encfile:
-					encfile.write(String_data_enc.encode())
+					encfile.write(Enc_data)
 			with open('encoded.jpg','rb') as file:
 				data1 = s.get(url+"new/")
 				#print(*data1.cookies)
@@ -127,15 +79,11 @@ def req_new():
 def req(event=None):
 	try:
 		
-		img = cv2.imread("output.jpg")
-		img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-		cv2.imwrite("output.jpg",img)
 		with open('output.jpg','rb') as file:
 			Org_data = file.read()
-			String_data=str(Org_data.hex())
-			String_data_enc=enc_rtrn(String_data)
+			Enc_data = fnet.encrypt(Org_data)
 			with open('encoded.jpg','wb') as encfile:
-				encfile.write(String_data_enc.encode())
+				encfile.write(Enc_data)
 		with open('encoded.jpg','rb') as file:
 			data1 = s.get(url+"match/")
 			#print(*data1.cookies)
@@ -155,17 +103,19 @@ def req(event=None):
 def capture(event=None):
 	cam = Picamera2()
 	cam.start_preview(None)
-	capture_config = cam.create_still_configuration(main={"format": 'RGB888'})
-	#cam.configure(capture_config)
+	capture_config = cam.create_still_configuration(main={"format":"XRGB8888"})
+	cam.configure(capture_config)
 	cam.controls.ExposureTime=100000
 	cam.controls.Contrast=31.0
 	cam.start_and_capture_file("output.jpg")
 	cam.close()
-	img = cv2.imread("output.jpg",cv2.IMREAD_GRAYSCALE)
+	img = cv2.imread("output.jpg")
+	img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 	cv2.imwrite("output.jpg",img)
 	img_temp = cv2.resize(img,(800,600))
 	cv2.imshow("Preview Window",img_temp)
-	
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
 	ResLabel.configure(text="Image Captured")
 	
    
@@ -187,5 +137,4 @@ ResLabel = tk.Label(window, font=("Tahoma", 18))
 ResLabel.pack(fill='both',expand = True)
 
 window.bind( "<Return>" , capture )
-
 window.mainloop()
